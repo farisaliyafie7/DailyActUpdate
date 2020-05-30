2c6b090176a23126c5cbbb062dbc94bd322a3267
Faris Ali Yafie <farisaliyafie@gmail.com>
2020-05-30 17:37:38 +0700

    initial commit

---
+++ DailyAct/HistoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryViewController.swift
+//  DailyActUpdate
+//
+//  Created by Faris Ali Yafie on 30/05/20.
+//  Copyright © 2020 Faris Ali Yafie. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
